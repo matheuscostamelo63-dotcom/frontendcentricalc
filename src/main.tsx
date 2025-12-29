b67107f8a16b56034c3b611f9ecd286b98a478bc
[dyad] <matheuscostamelo63@gmail.com>
2025-12-29 12:48:29 -0300

    [dyad] Removed React.StrictMode from main.tsx - wrote 1 file(s)

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,8 @@
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
-import React from "react";
+// import React from "react"; // Removed React import as StrictMode is no longer used
 
 createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <App />
 );
