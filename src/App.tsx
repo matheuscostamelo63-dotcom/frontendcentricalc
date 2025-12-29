268085b806ba54ba931c4ec0b7535f0a943251b0
[dyad] <matheuscostamelo63@gmail.com>
2025-12-29 14:07:54 -0300

    [dyad] Implemented SafeRoutes for robust portal cleanup and removed PortalCleanupProvider - wrote 2 file(s), deleted 1 file(s)

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,10 @@
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import { ThemeProvider } from "next-themes";
 import { Sidebar } from "@/components/Sidebar";
-import Index from "./pages/Index";
-import MeusProjetos from "./pages/MeusProjetos";
-import Sobre from "./pages/Sobre";
-import ProjectDetails from "./pages/ProjectDetails";
-import NotFound from "./pages/NotFound";
-import { PortalCleanupProvider } from "@/components/PortalCleanupProvider";
+import { SafeRoutes } from "@/components/SafeRoutes";
 
 const queryClient = new QueryClient();
 
@@ -17,24 +12,15 @@ const App = () => (
   <QueryClientProvider client={queryClient}>
     <ThemeProvider attribute="class" defaultTheme="light" enableSystem={false}>
       <BrowserRouter>
-        <PortalCleanupProvider>
-          <TooltipProvider delayDuration={300}>
-            <Sonner />
-            <div className="flex min-h-screen w-full bg-background">
-              <Sidebar />
-              <main className="flex-1 lg:ml-64 pt-4 md:pt-8">
-                <Routes>
-                  <Route path="/" element={<Index />} />
-                  <Route path="/meus-projetos" element={<MeusProjetos />} />
-                  <Route path="/meus-projetos/:id" element={<ProjectDetails />} />
-                  <Route path="/sobre" element={<Sobre />} />
-                  {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
-                  <Route path="*" element={<NotFound />} />
-                </Routes>
-              </main>
-            </div>
-          </TooltipProvider>
-        </PortalCleanupProvider>
+        <TooltipProvider delayDuration={300}>
+          <Sonner />
+          <div className="flex min-h-screen w-full bg-background">
+            <Sidebar />
+            <main className="flex-1 lg:ml-64 pt-4 md:pt-8">
+              <SafeRoutes />
+            </main>
+          </div>
+        </TooltipProvider>
       </BrowserRouter>
     </ThemeProvider>
   </QueryClientProvider>
